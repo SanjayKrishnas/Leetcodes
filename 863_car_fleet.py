91d74de9ed0c061594cf80c2f8fe6728ee1238f8
SanjayKrishnas <sanjay.krishnas@gmail.com>
2025-04-01 17:34:54 -0700

    Submitted Leetcode 853

--- 863_car_fleet.py
+++ 863_car_fleet.py
@@ -3,42 +3,22 @@ from typing import List
 
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
-        #minimum speed of any car in the fleet
-        #this means if the cars become a fleet then they go at the speed of the slowest car
-        #so at each interval we need to see if two cars are at the same position at any point of time
+        #so basically each time one of the cars catches up to  (from behind to front) then we have a fleet
         result = 0
-        complete = 0
 
-        while complete < len(position):
-            #update positions
-            pos_spd = defaultdict(list)
-            for i in range(len(position)):
-                pos = position[i]
-                spd = speed[i]
-                if pos >= target: continue
+        position_index = [(pos, i) for i, pos in enumerate(position)]
+        position_index.sort(reverse=True)
 
-                #increment all the spots
-                position[i] += speed[i]
-                pos_spd[position[i]].append(i)
+        prev = 0
+        for i in range(len(position_index)):
+            pos = position_index[i][0]
+            spd = speed[position_index[i][1]]
 
-            #check completions
-            for key, val in pos_spd.items():
-                if key >= target:
-                    result += 1
-                    complete += len(val)
+            time = (target - pos) / spd
 
-            #update speeds accordingly
-            for key, val in pos_spd.items():
-                rel_min = speed[val[0]]
+            #if the car directly behind the prev car can't catch up then NO car can catch up guaranteed since the speeds can only get lower
+            if time > prev: #if the car can't catch up then a fleet has been formed
+                result += 1
+                prev = time
 
-                for i in val: #get min speed
-                    rel_min = min(rel_min, speed[i])
-                
-                for i in val: #reassign the min speed
-                    speed[i] = rel_min
-            
-            #now we want to check if any of them are the same
-
-        print(position)
-        print(speed)
         return result
